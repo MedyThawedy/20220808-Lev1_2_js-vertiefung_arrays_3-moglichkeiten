7fbca3a6ec3c240c29352322de2dd4972da3dfac
MD <dhmedy@googlemail.com>
2022-08-08 18:16:38 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/* Erstelle ein Array meineTrainer1 mit den Namen der Trainer*innen: Eric, Steffen, Simon, Ahmed. Gib es in der Konsole aus. */
+
+let meineTrainer1 = ["Max", "Steffen", "Simon", "Ahmed"];
+console.log(meineTrainer1);
+
+let meineTrainer2 = new Array("Max", "Steffen", "Simon", "Ahmed");
+console.log(meineTrainer2);
+
+let meineTrainer3 = new Array();
+meineTrainer3[0] = "Eric";
+meineTrainer3[1] = "Steffen";
+meineTrainer3[2] = "Simon";
+meineTrainer3[3] = "Ahmed";
+console.log(meineTrainer3);
+
